acf4c926ee059f1106463161f892152b03aed4e0
Edisemiefegi <edisemiefegi@gmail.com>
2025-12-18 20:14:52 +0100

    first commit

---
+++ appwriteConfig.ts
@@ -0,0 +1,11 @@
+import { Client, Account} from 'appwrite';
+
+export const client = new Client();
+const url = (import.meta as any).env.APPWRITE_ENDPOINT;
+const projectId = (import.meta as any).env.APPWRITE_PROJECT_ID;
+
+client
+    .setEndpoint(url)
+    .setProject(projectId); 
+export const account = new Account(client);
+export { ID } from 'appwrite';
