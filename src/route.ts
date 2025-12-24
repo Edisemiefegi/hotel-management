d5b21c243a175a578101188b7b9d661240d85863
Edisemiefegi <edisemiefegi@gmail.com>
2025-12-24 21:54:57 +0100

    enum removed

--- src/route.ts
+++ src/route.ts
@@ -1,8 +1,8 @@
-export enum ROUTES {
-  Dashboard= "/admin/dashboard",
-  Hotels= "/admin/hotels",
-  Rooms= "/admin/rooms",
-  Bookings= "/admin/bookings",
-  Clients= "/admin/clients",
-  Settings= "/admin/settings",
-} 
+export const ROUTES = {
+  Dashboard: "/admin/dashboard",
+  Hotels: "/admin/hotels",
+  Rooms: "/admin/rooms",
+  Bookings: "/admin/bookings",
+  Clients: "/admin/clients",
+  Settings: "/admin/settings",
+} as const;
