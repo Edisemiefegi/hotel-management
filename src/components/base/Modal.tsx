146d2f37b1be0605d9d926fc14351a70db057960
Whyte Peter <peteremmanuelwhyte@gmail.com>
2026-01-18 18:25:55 +0100

    modal refactored

--- src/components/base/Modal.tsx
+++ src/components/base/Modal.tsx
@@ -1,16 +1,18 @@
 import type { ReactNode } from "react";
-import Card from "./Card";
 import { X } from "lucide-react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 
 interface Props {
   children: ReactNode;
+  title?: string | ReactNode;
+  description?: string | ReactNode;
+  titleClassName?: string;
+  descriptionClassName?: string;
   noHeader?: boolean;
   noFooter?: boolean;
   fullScreen?: boolean;
   className?: string;
-  containerClassName?: string;
   contentClassName?: string;
   headerClassName?: string;
   footerClassName?: string;
@@ -19,35 +21,43 @@ interface Props {
   onClose?: () => void;
 }
 
-function Modal({ children, onClose, noHeader = true, noFooter = true, containerClassName, contentClassName, fullScreen, className, headerClassName, footerClassName, headerContent, footerContent }: Props) {
+function Modal({ children, onClose, noHeader = false, noFooter = false, contentClassName, title, description, titleClassName, descriptionClassName, fullScreen, className, headerClassName, footerClassName, headerContent, footerContent }: Props) {
+
+
+
   return (
-    <div className="fixed inset-0 z-50 h-full  flex items-center justify-center">
+    <div className="fixed inset-0 z-50 h-screen w-screen  flex items-center justify-center">
       <div className="absolute inset-0 bg-black/30  backdrop-blur-xs"></div>
 
-      <div className={cn(" z-10 w-full max-w-lg mx-4", containerClassName)}>
-        <Card className={cn("bg-white relative  h-[600px] overflow-y-auto ", className)}>
-          {/* ///Header //// */}
-          {!noHeader && <div className={cn('sticky left-0 top-0 border-4', headerClassName)}>
-            {headerContent ? headerContent : <>
-              <h2>Title</h2>
-              <p>Description</p>
-            </>}
-          </div>}
-          <Button
-            variant={"ghost"}
-            onClick={onClose}
-            className="absolute  right-2 top-2"
-          >
-            <X size={20} />
-          </Button>
-          <div className={cn("", contentClassName)}>
-            {children}
-          </div>
-          {/* ///Footer //// */}
-          {!noFooter && <div className={cn("sticky left-0 bottom-0 border-4", footerClassName)}>
-            {footerContent}
-          </div>}
-        </Card>
+      <div className={cn("z-10 bg-white flex flex-col gap-4 p-4 md:px-5 relative ",
+        fullScreen ? "h-screen w-screen" : " mx-4 w-full rounded-xl max-w-lg h-[600px]",
+        className)}>
+
+        {/* ///Header //// */}
+        {!noHeader && <div className={cn('sticky left-0 top-0 bg-white ', headerClassName)}>
+          {headerContent ? headerContent : <>
+            {title && <h2 className={cn("text-lg font-semibold", titleClassName)}>{title}</h2>}
+            {description && <p className={cn("text-sm text-gray-500", descriptionClassName)}>{description}</p>}
+
+          </>}
+        </div>}
+
+        <Button
+          variant={"ghost"}
+          onClick={onClose}
+          className="absolute right-2 top-2"
+        >
+          <X size={20} />
+        </Button>
+
+        <div className={cn(" flex-1 overflow-y-auto", contentClassName)}>
+          {children}
+        </div>
+        {/* ///Footer //// */}
+        {!noFooter && <div className={cn("sticky left-0 bottom-0 bg-white", footerClassName)}>
+          {footerContent}
+        </div>}
+
       </div>
     </div>
   );
--- src/components/hotel/AddHotel.tsx
+++ src/components/hotel/AddHotel.tsx
@@ -4,11 +4,15 @@ import HotelForm from "./HotelForm";
 import { useState } from "react";
 import type { Hotel, } from "@/types";
 import Modal from "../base/Modal";
+import { useBreakpoint } from "@/hooks/useBreakpoint";
 
 interface Props {
   onClose?: () => void;
 }
 function AddHotel({ onClose }: Props) {
+  const { isSmallerOrEqualTo } = useBreakpoint()
+
+
   const { addHotel } = useAdmin();
   const [loading, setLoading] = useState(false);
 
@@ -30,19 +34,22 @@ function AddHotel({ onClose }: Props) {
   };
 
   return (
-    <Modal onClose={onClose}>
-      <div className="space-y-4">
-        <p className="font-semibold text-lg">Add New Hotel</p>
-        <HotelForm data={form} onChange={setForm} />
-        <div className="flex justify-end gap-4">
+    <Modal
+      fullScreen={isSmallerOrEqualTo("md")}
+      onClose={onClose}
+      title="Add New Hotel"
+      footerContent={
+        <div className="grid grid-cols-2 md:flex justify-end gap-4">
           <Button variant={"outline"} onClick={onClose}>
             Cancel
           </Button>
           <Button disabled={loading} onClick={handleSubmit}>
             {loading ? "loading..." : "Add Hotel"}
           </Button>
-        </div>{" "}
-      </div>
+        </div>
+      }>
+
+      <HotelForm data={form} onChange={setForm} />
     </Modal>
   );
 }
